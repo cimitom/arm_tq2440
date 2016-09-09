e3dfdc453fdbdd551d9eb185a77e3b68def45df0
elvinsys <jianlongliang@qq.com>
2016-09-09 17:18:56 +0800

    add core

---
+++ 2_app/2-3.coredump/test_a.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(int argc, char ** argv)
+{
+    char *ptr = "123456";
+    ptr[0] = 1;
+    return 0;
+}
---
+++ 2_app/2-3.coredump/test_b.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(int argc, char ** argv)
+{
+    int *ptr = NULL;
+    *ptr = 0;
+    return 0;
+}
